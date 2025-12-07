e028d6a22de36fa9831608ff947ebbbe04d78852
jawarianoor <loyalsaadkhan05@gmail.com>
2025-12-06 22:35:13 -0800

    Physical AI Humanoid Robotics Textbook

---
+++ docusaurus_textbook/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\Jawaria Noor\\Documents\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Jawaria Noor\\Documents\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Jawaria Noor\\Documents\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\Jawaria Noor\\Documents\\hackathon-physical-ai-humanoid-textbook-main\\docusaurus_textbook\\src\\css\\custom.css"),
+];
